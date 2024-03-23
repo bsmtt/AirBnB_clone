1a84a40fe265cf13df06c7ef8414d949eb8a076b
bsmtt <basmatalaat224@gmail.com>
2024-03-22 18:13:27 -0700

    feat: test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""" Test cases for the base model"""
+
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Unittests for testing the BaseModel class."""
+
+    def setUp(self):
+        """
+        testing
+        """
+        pass
+
+    def test_id_is_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_type(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_two_models_diff_ids(self):
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+    
