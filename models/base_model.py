0631bbb6dfebb9ff5393e797cb87fcc25d66f254
bsmtt <basmatalaat224@gmail.com>
2024-03-25 15:57:31 -0700

    fix: str model

--- models/base_model.py
+++ models/base_model.py
@@ -27,8 +27,8 @@ class BaseModel:
 
     def __str__(self):
         """ string representation of the class. """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        self_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(self_name, self.id, self.__dict__)
 
     def save(self):
         """ save to instance. """
